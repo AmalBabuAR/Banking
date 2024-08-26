422c96ecc848125b83e645324a0d6514f8f3c168
AmalBabuAR <amalbabuar@gmail.com>
2024-08-26 12:12:59 +0530

    implement auth, appwrite and sentry

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,13 @@
 import HeaderBox from "@/components/HeaderBox";
-import RightSidebar from "@/components/RightSIdebar";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const Home = () => {
-  const loggedIn = {
-    firstName: "Amal",
-    lastName: "Babu",
-    email: "amalbabau@gmail",
-  };
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
+ 
+
   return (
     <section className="home">
       <div className="home-content">
@@ -16,7 +15,7 @@ const Home = () => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || "Guest"}
+            user={loggedIn?.name || "Guest"}
             subtext="Access and manage your account and transactions efficiently"
           />
 
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://8faa6cd906cab7929b12bea5f9db339f@o4507842195226624.ingest.us.sentry.io/4507842196668416",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
